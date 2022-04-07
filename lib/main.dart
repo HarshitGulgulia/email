fea9423c31f296110ae7f1ed1ad4f43e98b25f51
codewithkushagra <kushagraagra008@gmail.com>
2022-04-07 19:47:49 +0530

    added google authentication

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,10 @@
 import 'package:email_client/screens/loading_screen.dart';
+import 'package:email_client/screens/login_screen.dart';
+import 'package:email_client/screens/login_wrapper.dart';
 import 'package:email_client/screens/main/main_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:email_client/services/get_mail.dart';
 
-GetMail mails= GetMail();
 
 void main() => runApp(const MyApp());
 
@@ -32,49 +33,40 @@ class _MyStatefulWidgetState extends State<MyStatefulWidget> {
 
   @override
   Widget build(BuildContext context) {
-    return DefaultTextStyle(
-      style: Theme.of(context).textTheme.headline2,
-      textAlign: TextAlign.center,
-      child: FutureBuilder<String>(
-        future: mails.getEmail(), // a previously-obtained Future<String> or null
-        builder: (BuildContext context, AsyncSnapshot<String> snapshot) {
-          List<Widget> children;
-          if (snapshot.hasData) {
-            return MainScreen(mails.emails);
-          } else if (snapshot.hasError) {
-            children = <Widget>[
-              const Icon(
-                Icons.error_outline,
-                color: Colors.red,
-                size: 60,
-              ),
-              Padding(
-                padding: const EdgeInsets.only(top: 16),
-                child: Text('Error: ${snapshot.error}'),
-              )
-            ];
-          } else {
-            return Loader();
-            // children = const <Widget>[
-            //   SizedBox(
-            //     width: 60,
-            //     height: 60,
-            //     child: CircularProgressIndicator(),
-            //   ),
-            //   Padding(
-            //     padding: EdgeInsets.only(top: 16),
-            //     child: Text('Awaiting result...'),
-            //   )
-            // ];
-          }
-          return Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: children,
-            ),
-          );
-        },
-      ),
-    );
+    return LoginWrapper();
   }
 }
+
+// DefaultTextStyle(
+// style: Theme.of(context).textTheme.headline2,
+// textAlign: TextAlign.center,
+// child: FutureBuilder<String>(
+// future: mails.getEmail(), // a previously-obtained Future<String> or null
+// builder: (BuildContext context, AsyncSnapshot<String> snapshot) {
+// List<Widget> children;
+// if (snapshot.hasData) {
+// return LoginScreen(emails: mails.emails);
+// } else if (snapshot.hasError) {
+// children = <Widget>[
+// const Icon(
+// Icons.error_outline,
+// color: Colors.red,
+// size: 60,
+// ),
+// Padding(
+// padding: const EdgeInsets.only(top: 16),
+// child: Text('Error: ${snapshot.error}'),
+// )
+// ];
+// } else {
+// return Loader();
+// }
+// return Center(
+// child: Column(
+// mainAxisAlignment: MainAxisAlignment.center,
+// children: children,
+// ),
+// );
+// },
+// ),
+// );
