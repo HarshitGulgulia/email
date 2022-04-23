f5a73bcbdfe8acb7172887b80801eaa99443022d
HarshitGulgulia <harshitgulgulia@gmail.com>
2022-04-23 19:06:07 +0530

    made some chnages in UI

--- lib/screens/email/components/email_screen_details.dart
+++ lib/screens/email/components/email_screen_details.dart
@@ -16,49 +16,86 @@ class EmailScreenDetails extends StatefulWidget {
 class _EmailScreenDetailsState extends State<EmailScreenDetails> {
   @override
   Widget build(BuildContext context) {
-    return Row(
+    return Column(
       children: [
-        CircleAvatar(
-          maxRadius: 24,
-          backgroundColor: Colors.transparent,
-          backgroundImage: AssetImage(widget.email.image),
-        ),
-        SizedBox(width:kDefaultPadding/2),
-        Expanded(
-          child: Column(
-            crossAxisAlignment:
-            CrossAxisAlignment.start,
-            children: [
-              Text.rich(
-                TextSpan(
-                  text: widget.email.name,
-                  style: Theme.of(context)
-                      .textTheme
-                      .button,
-                  children: [
-                    TextSpan(
-                        text:
-                        widget.email.from_email,
-                        style: Theme.of(context)
-                            .textTheme
-                            .caption),
-                  ],
-                ),
+        Row(
+          children: [
+            CircleAvatar(
+              maxRadius: 24,
+              backgroundColor: Colors.transparent,
+              backgroundImage: AssetImage(widget.email.image),
+            ),
+            SizedBox(width: kDefaultPadding / 2),
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    widget.email.name,
+                    style: TextStyle(
+                      fontSize: 18
+                    ),
+                    //style: Theme.of(context).textTheme.button,
+                  ),
+                  Text(
+                    widget.email.from_email,
+                    style: Theme.of(context).textTheme.caption,
+                  ),
+                  // Row(
+                  //   children: [
+                  //     Text(
+                  //       widget.email.name,
+                  //       style: Theme.of(context)
+                  //           .textTheme
+                  //           .button,
+                  //     ),
+                  //     // SizedBox(width: kDefaultPadding/10),
+                  //     Text(
+                  //         widget.email.from_email,
+                  //         style: Theme.of(context)
+                  //             .textTheme
+                  //             .caption,
+                  //     ),
+                  //   ],
+                  // ),
+                  // Text.rich(
+                  //   TextSpan(
+                  //     text: widget.email.name,
+                  //     style: Theme.of(context)
+                  //         .textTheme
+                  //         .button,
+                  //     children: [
+                  //       TextSpan(
+                  //           text:
+                  //           widget.email.from_email,
+                  //           style: Theme.of(context)
+                  //               .textTheme
+                  //               .caption),
+                  //     ],
+                  //   ),
+                  // ),
+                  // Text(
+                  //   widget.email.subject,
+                  //   style: Theme.of(context).textTheme.headline6,
+                  // )
+                ],
               ),
-              Text(
-                widget.email.subject,
-                style: Theme.of(context)
-                    .textTheme
-                    .headline6,
-              )
-            ],
-          ),
+            ),
+            SizedBox(width: kDefaultPadding / 2),
+            Text(
+              widget.email.time,
+              style: Theme.of(context).textTheme.caption,
+            ),
+            SizedBox(width: kDefaultPadding / 2),
+          ],
         ),
-        SizedBox(width: kDefaultPadding / 2),
-        Text(
-          widget.email.time,
-          style:
-          Theme.of(context).textTheme.caption,
+        SizedBox(height: kDefaultPadding),
+        Align(
+          alignment: Alignment.centerLeft,
+          child: Text(
+            widget.email.subject,
+            style: Theme.of(context).textTheme.headline6,
+          ),
         ),
       ],
     );
